62ab35c4e1b4f70fe252c8892606a280ff697b6e
Ege <egeege95@gmail.com>
2022-07-16 14:43:37 +0300

    Initial Commit

---
+++ PocketNews/PocketNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketNewsApp.swift
+//  PocketNews
+//
+//  Created by Ege Çam on 16.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct PocketNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
