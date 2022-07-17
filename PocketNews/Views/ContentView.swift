46f2d7341a8274139118e093835b0c405213123a
Ege <egeege95@gmail.com>
2022-07-17 14:37:57 +0300

    initial commit

---
+++ PocketNews/Views/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  PocketNews
+//
+//  Created by Ege Çam on 16.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bonjour"),
+//    Post(id: "3", title: "Merhaba")
+//]
