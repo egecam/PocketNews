f29ea72b8f652688487540e746810b010d90f4b4
Ege <egeege95@gmail.com>
2022-07-17 15:01:29 +0300

    create and extract WebView component

---
+++ PocketNews/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  PocketNews
+//
+//  Created by Ege Çam on 17.07.2022.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
