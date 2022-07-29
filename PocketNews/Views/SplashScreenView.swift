4dfc7e4762d9ad988f734725ed947f17904d9069
Ege <egeege95@gmail.com>
2022-07-29 12:56:28 +0300

    defined royalblue color

--- PocketNews/Views/SplashScreenView.swift
+++ PocketNews/Views/SplashScreenView.swift
@@ -12,6 +12,7 @@ struct SplashScreenView: View {
     @State private var isActive = false
     @State private var size = 0.8
     @State private var opacity = 0.5
+    @State private var royalblue = UIColor(named: "royalblue")
     
     var body: some View {
         if isActive {
